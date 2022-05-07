380a748ec80d9210f6fd88de4c220593912f5c9b
fardous hridoy <mdreday736@gmail.com>
2022-05-08 01:32:44 +0600

    post added

--- src/Page/AddItem/AddItem.js
+++ src/Page/AddItem/AddItem.js
@@ -3,12 +3,31 @@ import { useForm } from "react-hook-form";
 
 const AddItem = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/item`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result =>{
+                console.log(result);
+            
+        })
+
+        
+    };
     return (
         <div className='w-50 mx-auto '>
             <h2>Please Add Item</h2>
             <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-                <input className='mb-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+                <input className='mb-2' placeholder='Name' {...register("name")} />
+                <input className='mb-2' placeholder='Supplier' {...register("supplier")} />
                 <textarea className='mb-2' placeholder='Description' {...register("description")} />
                 <input className='mb-2' placeholder='price' type="number" {...register("price")} />
                 <input className='mb-2' placeholder='Quantity' type="number" {...register("quantity")} />
