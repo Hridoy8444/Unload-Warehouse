84ae5b2f1efdf5f8a1081fd1f0efd3378aa3448e
fardous hridoy <mdreday736@gmail.com>
2022-05-07 21:07:26 +0600

    input feild added in item detail

--- src/Page/ItemDetail/ItemDetail.js
+++ src/Page/ItemDetail/ItemDetail.js
@@ -3,22 +3,39 @@ import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const ItemDetail = () => {
-    const {itemId} =useParams();
+    const { itemId } = useParams();
     const [item, setItem] = useState({});
 
     useEffect(() => {
         const url = `http://localhost:5000/item/${itemId}`;
         fetch(url)
-        .then(res => res.json())
-        .then(data => setItem(data));
+            .then(res => res.json())
+            .then(data => setItem(data));
 
 
     }, [])
     return (
         <div>
-            <h2>welcome to detail: {item.name}</h2>
+            <h2>Item detail</h2>
+            <div className='d-flex'>
+                <div>
+                    <img src={item.img} alt="" />
+                    <p>Name: {item.name}</p>
+                    <p>Supplier Name: {item.supplier}</p>
+                    <p>Available Item: {item.quantity}</p>
+                    <p><small>{item.description}</small></p>
+                    <p>Price: {item.price}</p>
+                    <Button className='btn'>Delivered</Button>
 
-            <Button className='btn '>Proceed CheckOut</Button>
+                </div>
+                <div>
+                    <input className='mb-1 ' type='number' pattern='[0-9]'></input>
+                    <input className='mb-1 pe-5 ps-5' type="submit" value="ReStock" />
+                    <Button>Manage Inventory</Button>
+                </div>
+            </div>
+
+            
         </div>
     );
 };
